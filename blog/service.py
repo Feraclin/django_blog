b56e5c065466e4fef61078fc3fa71d67a6c91410
feraclin <feraclin@gmail.com>
2022-11-16 06:53:49 +0300

    change bulk_create

--- blog/service.py
+++ blog/service.py
@@ -7,14 +7,7 @@ class FeedPost:
 
     @staticmethod
     def create_post(users_lst: List[CustomUser], post: Post) -> None:
-        for user in users_lst:
-            feed_post = Feed(recipient=user,
-                             post=post)
-            feed_post.save()
-
-    @staticmethod
-    def postslist(user: CustomUser) -> List[Post]:
-        return Feed.objects.filter(recipient=user)
+        Feed.objects.bulk_create([Feed(recipient=user, post=post) for user in users_lst])
 
 
 post_editor = FeedPost()
